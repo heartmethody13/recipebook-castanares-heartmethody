f64e8cc3f80bee9916b15cb9ca87ff2c74fd72d6
heartmethody13 <heart.castanares@student.ateneo.edu>
2024-02-15 23:07:19 +0800

    Edited out lines unrelated to the lab.

--- ledger/views.py
+++ ledger/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
-def index(request):
-    return HttpResponse('Hello World')
 
 def recipe_list(request):
     ctx = {
