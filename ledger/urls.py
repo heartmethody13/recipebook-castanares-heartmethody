10d27bbf5a8c3e45670c8078cac9dcf2b88bde77
heartmethody13 <heart.castanares@student.ateneo.edu>
2024-02-15 23:14:05 +0800

    Code cleaning

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import index, recipe_list, first_recipe, second_recipe
 
+
 urlpatterns = [
     path('recipes/list', recipe_list, name = '/recipes/list'),
     path('recipe/1', first_recipe, name = '/recipe/1'),
