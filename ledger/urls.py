6cb7bf33653e63fd5e2d15309e34c88f3be14c02
heartmethody13 <heart.castanares@student.ateneo.edu>
2024-02-15 23:03:25 +0800

    Uploading files to existing Github repository

---
+++ ledger/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import index, recipe_list, first_recipe, second_recipe
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('recipes/list', recipe_list, name = '/recipes/list'),
+    path('recipe/1', first_recipe, name = '/recipe/1'),
+    path('recipe/2', second_recipe, name = '/recipe/2')
+]
+
+app_name = "ledger"
